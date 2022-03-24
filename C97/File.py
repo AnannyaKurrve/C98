9d3cdb1513f645f9c5c04d31d2235f0535a5bdb8
AnannyaKurrve <79566894+AnannyaKurrve@users.noreply.github.com>
2022-03-24 21:31:01 +0530

    Add files via upload

---
+++ C97/File.py
@@ -0,0 +1,12 @@
+number = 7
+
+YourNumber=input("Enter your favourite nuber between 1-14 : ")
+
+if (YourNumber>7):
+    print ("Too big")
+
+elif (YourNumber<7):
+    print("Too small")
+
+else:
+    print("You Win")
---
+++ C97/File2.py
@@ -0,0 +1,3 @@
+number = 7
+
+if guess == nu
